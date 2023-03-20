585521f67f078191939464b671d6ae964f2b9ceb
Kostina Julia <julik280201@mail.ru>
2023-03-20 23:04:33 +0300

    Инициализация проекта

---
+++ SchoolEnglish.UI/school_english/android/app/src/main/kotlin/com/example/school_english/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.school_english
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ SchoolEnglish.UI/school_english/lib/constants.dart
@@ -0,0 +1,2 @@
+const mobileWidth = 640.0;
+const tabletWidth = 1100.0;
