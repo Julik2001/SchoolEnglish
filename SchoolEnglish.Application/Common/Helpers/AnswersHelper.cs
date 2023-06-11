aeebf3364f8bd4940f285bd3635817f637bfd0c0
Kostina Julia <julik280201@mail.ru>
2023-06-11 15:42:47 +0300

    Добавлен класс для проверки ответов

---
+++ SchoolEnglish.Application/Common/Helpers/AnswersHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace SchoolEnglish.Application.Common.Helpers
+{
+    public static class AnswersHelper
+    {
+        public static bool CheckAnswer(string answer, string rightAnswer) =>
+            answer == rightAnswer || 
+            ToUpperFirstLetter(answer) == ToUpperFirstLetter(rightAnswer);
+
+        private static string ToUpperFirstLetter(string source)
+        {
+            if (string.IsNullOrEmpty(source))
+                return string.Empty;
+
+            var letters = source.ToCharArray();
+            if (letters.Length > 0)
+            {
+                letters[0] = char.ToUpper(letters[0]);
+            }
+
+            return new string(letters);
+        }
+    }
+}
